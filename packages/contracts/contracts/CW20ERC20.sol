e733ad050f91061108be14febc8627a5aa61aa0f
perfogic <longdieu12x@gmail.com>
2025-03-05 16:02:14 +0700

    chore: fix compile CW20ERC20.sol

--- packages/contracts/contracts/CW20ERC20.sol
+++ packages/contracts/contracts/CW20ERC20.sol
@@ -4,88 +4,37 @@ pragma solidity ^0.8.0;
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/utils/Strings.sol";
-import {IWasmd, WASMD_PRECOMPILE_ADDRESS} from "./precompiles/IWasmd.sol";
-import {IJson, JSON_PRECOMPILE_ADDRESS} from "./precompiles/IJson.sol";
-import {IAddr, ADDR_PRECOMPILE_ADDRESS} from "./precompiles/IAddr.sol";
-import {IBank, BANK_PRECOMPILE_ADDRESS} from "./precompiles/IBank.sol";
+import {IWasmd} from "./precompiles/IWasmd.sol";
+import {IJson} from "./precompiles/IJson.sol";
+import {IAddr} from "./precompiles/IAddr.sol";
+import {IBank} from "./precompiles/IBank.sol";
 
 contract CW20ERC20Token is IERC20, Ownable {
+    address constant WASMD_PRECOMPILE_ADDRESS =
+        0x9000000000000000000000000000000000000001;
+    address constant JSON_PRECOMPILE_ADDRESS =
+        0x9000000000000000000000000000000000000002;
+    address constant ADDR_PRECOMPILE_ADDRESS =
+        0x9000000000000000000000000000000000000003;
+    address constant BANK_PRECOMPILE_ADDRESS =
+        0x9000000000000000000000000000000000000004;
     IWasmd public WasmdPrecompile;
     IJson public JsonPrecompile;
     IAddr public AddrPrecompile;
     IBank public BankPrecompile;
-    string public Cw20Address;
+    string public cw20Address;
 
     constructor(
-        string memory Cw20Address_
-    ) Ownable(msg.sender) {
+        string memory cw20Address_,
+        address _owner
+    ) Ownable(_owner) {
         WasmdPrecompile = IWasmd(WASMD_PRECOMPILE_ADDRESS);
         JsonPrecompile = IJson(JSON_PRECOMPILE_ADDRESS);
         AddrPrecompile = IAddr(ADDR_PRECOMPILE_ADDRESS);
         BankPrecompile = IBank(BANK_PRECOMPILE_ADDRESS);
-        Cw20Address = Cw20Address_;
+        cw20Address = cw20Address_;
     }
 
-    function name() public view returns (string memory) {
-        // string memory req = _curlyBrace(_formatPayload("token_info", "{}"));
-        // bytes memory response = WasmdPrecompile.query(Cw20Address, bytes(req));
-        return "name";
-    }
-
-    function symbol() public view returns (string memory) {
-        // string memory req = _curlyBrace(_formatPayload("token_info", "{}"));
-        // bytes memory response = WasmdPrecompile.query(Cw20Address, bytes(req));
-        return "symbol";
-    }
-
-    // Queries
-    function decimals() public view returns (uint8) {
-        string memory req = _curlyBrace(_formatPayload("token_info", "{}"));
-        bytes memory response = WasmdPrecompile.query(Cw20Address, bytes(req));
-        return uint8(JsonPrecompile.extractAsUint256(response, "decimals"));
-    }
-
-    function balanceOf(address owner) public view returns (uint256) {
-        require(
-            owner != address(0),
-            "ERC20: balance query for the zero address"
-        );
-        string memory ownerAddr = _formatPayload(
-            "address",
-            _doubleQuotes(AddrPrecompile.getCosmosAddr(owner))
-        );
-        string memory req = _curlyBrace(
-            _formatPayload("balance", _curlyBrace(ownerAddr))
-        );
-        bytes memory response = WasmdPrecompile.query(Cw20Address, bytes(req));
-        return JsonPrecompile.extractAsUint256(response, "balance");
-    }
-
-    function totalSupply() public view returns (uint256) {
-        string memory req = _curlyBrace(_formatPayload("token_info", "{}"));
-        bytes memory response = WasmdPrecompile.query(Cw20Address, bytes(req));
-        return JsonPrecompile.extractAsUint256(response, "total_supply");
-    }
-
-    // Transactions
-    function transfer(address to, uint256 amount) public returns (bool) {
-        require(to != address(0), "ERC20: transfer to the zero address");
-        string memory recipient = _formatPayload(
-            "recipient",
-            _doubleQuotes(AddrPrecompile.getCosmosAddr(to))
-        );
-        string memory amt = _formatPayload(
-            "amount",
-            _doubleQuotes(Strings.toString(amount))
-        );
-        string memory req = _curlyBrace(
-            _formatPayload("transfer", _curlyBrace(_join(recipient, amt, ",")))
-        );
-        _execute(req);
-        return true;
-    }
-
-    // Make as an optional ?
     function mint(address account, uint256 amount) external onlyOwner {
         require(account != address(0), "ERC20: mint to  the zero address");
         string memory recipient = _formatPayload(
@@ -102,10 +51,71 @@ contract CW20ERC20Token is IERC20, Ownable {
         _execute(req);
     }
 
+    function name() public view returns(string memory) {
+        string memory req = _curlyBrace(_formatPayload("token_info", "{}"));
+        bytes memory response = WasmdPrecompile.query(cw20Address, bytes(req));
+        return string(JsonPrecompile.extractAsBytes(response, "name"));
+    }
+
+    function symbol() public view returns(string memory) {
+        string memory req = _curlyBrace(_formatPayload("token_info", "{}"));
+        bytes memory response = WasmdPrecompile.query(cw20Address, bytes(req));
+        return string(JsonPrecompile.extractAsBytes(response, "symbol"));
+    }
+
+    function decimals() public view returns (uint8) {
+        string memory req = _curlyBrace(_formatPayload("token_info", "{}"));
+        bytes memory response = WasmdPrecompile.query(cw20Address, bytes(req));
+        return uint8(JsonPrecompile.extractAsUint256(response, "decimals"));
+    }
+
+    function balanceOf(address owner) public view override returns (uint256) {
+        require(
+            owner != address(0),
+            "ERC20: balance query for the zero address"
+        );
+        string memory ownerAddr = _formatPayload(
+            "address",
+            _doubleQuotes(AddrPrecompile.getCosmosAddr(owner))
+        );
+        string memory req = _curlyBrace(
+            _formatPayload("balance", _curlyBrace(ownerAddr))
+        );
+        bytes memory response = WasmdPrecompile.query(cw20Address, bytes(req));
+        return JsonPrecompile.extractAsUint256(response, "balance");
+    }
+
+    function totalSupply() public view override returns (uint256) {
+        string memory req = _curlyBrace(_formatPayload("token_info", "{}"));
+        bytes memory response = WasmdPrecompile.query(cw20Address, bytes(req));
+        return JsonPrecompile.extractAsUint256(response, "total_supply");
+    }
+
+    // Transactions
+    function transfer(
+        address to,
+        uint256 amount
+    ) public override returns (bool) {
+        require(to != address(0), "ERC20: transfer to the zero address");
+        string memory recipient = _formatPayload(
+            "recipient",
+            _doubleQuotes(AddrPrecompile.getCosmosAddr(to))
+        );
+        string memory amt = _formatPayload(
+            "amount",
+            _doubleQuotes(Strings.toString(amount))
+        );
+        string memory req = _curlyBrace(
+            _formatPayload("transfer", _curlyBrace(_join(recipient, amt, ",")))
+        );
+        _execute(req);
+        return true;
+    }
+
     function allowance(
         address owner,
         address spender
-    ) public view returns (uint256) {
+    ) public view override returns (uint256) {
         require(
             owner != address(0),
             "ERC20: allowance query for the zero address"
@@ -124,20 +134,15 @@ contract CW20ERC20Token is IERC20, Ownable {
                 _curlyBrace(_join(ownerAddr, spenderAddr, ","))
             )
         );
-        bytes memory response = WasmdPrecompile.query(Cw20Address, bytes(req));
+        bytes memory response = WasmdPrecompile.query(cw20Address, bytes(req));
         return JsonPrecompile.extractAsUint256(response, "allowance");
-    }
-
-    function approve(address spender, uint256 amount) public returns (bool) {
-        _approve(msg.sender, spender, amount);
-        return true;
     }
 
     function transferFrom(
         address from,
         address to,
         uint256 amount
-    ) public returns (bool) {
+    ) public override returns (bool) {
         require(from != address(0), "ERC20: transfer from the zero address");
         require(to != address(0), "ERC20: transfer to the zero address");
         string memory fromAddr = _formatPayload(
@@ -162,7 +167,11 @@ contract CW20ERC20Token is IERC20, Ownable {
         return true;
     }
 
-    function _approve(address owner, address spender, uint256 amount) internal {
+    function approve(
+        address spender,
+        uint256 amount
+    ) public returns(bool) {
+        address owner = _msgSender();
         require(owner != address(0), "ERC20: approve from the zero address");
         require(spender != address(0), "ERC20: approve to the zero address");
 
@@ -201,6 +210,7 @@ contract CW20ERC20Token is IERC20, Ownable {
         _execute(req);
 
         emit Approval(owner, spender, amount);
+        return true;
     }
 
     function _execute(string memory req) internal returns (bytes memory) {
@@ -208,7 +218,7 @@ contract CW20ERC20Token is IERC20, Ownable {
             .delegatecall(
                 abi.encodeWithSignature(
                     "execute(string,bytes,bytes)",
-                    Cw20Address,
+                    cw20Address,
                     bytes(req),
                     bytes("[]")
                 )
