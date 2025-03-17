201e2e42c952d379a1f1c4333cfe58187c1a213a
ducphamle2 <ducphamle212@gmail.com>
2025-03-17 15:46:57 -0700

    chore: update readme & import mnemonic from account local

--- packages/contracts/hardhat.config.ts
+++ packages/contracts/hardhat.config.ts
@@ -4,12 +4,34 @@ import "hardhat-contract-sizer";
 import config from "dotenv";
 import { HardhatUserConfig } from "hardhat/config";
 import { ethers } from "ethers";
+import fs from "fs"; // Use promises API for async file reading
 config.config();
 
 let privateKeys: string[] = [];
 for (let i = 0; i < 20; i++) {
   const wallet = ethers.Wallet.createRandom();
   privateKeys = [...privateKeys, wallet.privateKey];
+}
+
+/**
+ * Extract mnemonic from account key file. Eg: account-eth.txt from wasmd when running ./scripts/setup_oraid.sh
+ * @param filePath - path to the account key file
+ * @returns mnemonic
+ */
+function extractMnemonicFromAccountKeyFile(filePath: string): string {
+  try {
+    // Read the file content synchronously as a string
+    const content = fs.readFileSync(filePath, "utf-8");
+
+    // Split the content into lines and filter out empty lines
+    const lines = content.split("\n").filter((line) => line.trim() !== "");
+
+    // Return the last line (the mnemonic)
+    return lines[lines.length - 1];
+  } catch (error) {
+    console.error("Error reading file:", error);
+    throw error; // Re-throw to handle it upstream if needed
+  }
 }
 
 const hardhatConfig: HardhatUserConfig = {
@@ -45,7 +67,12 @@ const hardhatConfig: HardhatUserConfig = {
       chainId: 3481324938,
       accounts: process.env.PRIVATE_KEY
         ? [process.env.PRIVATE_KEY || "", ...privateKeys]
-        : { mnemonic: process.env.MNEMONIC || "" },
+        : {
+            mnemonic:
+              process.env.MNEMONIC ||
+              extractMnemonicFromAccountKeyFile(process.env.ACCOUNT_KEY_FILE || "") ||
+              "",
+          },
     },
   },
 };
--- packages/contracts/scripts/deploy-and-send-native.ts
+++ packages/contracts/scripts/deploy-and-send-native.ts
@@ -1,5 +1,6 @@
 import hre from "hardhat";
-import { IBank__factory, NativeTokenSender__factory } from "../typechain-types";
+import { NativeTokenSender__factory } from "../typechain-types";
+import assert from "assert";
 const { ethers } = hre;
 
 const main = async () => {
@@ -26,20 +27,16 @@ const main = async () => {
 
   // Send native tokens
   const amountToSend = 100; // 0.0001 ORAI
+  const weiAmountToSend = amountToSend * 1e12;
   console.log("\nSending", amountToSend / 1e6, "ORAI to receiver...");
 
   // if send amountToSend to contract before, tx will success
-  //   await IBank__factory.connect(
-  //     "0x9000000000000000000000000000000000000004",
-  //     deployer
-  //   ).send(nativeTokenSender.target, "orai", amountToSend); // 0.000001 orai
-  //   await new Promise((resolve) => setTimeout(resolve, 1000));
 
   const sendTx = await nativeTokenSender.sendNativeTokens(
     deployer.address,
     amountToSend,
     {
-      value: amountToSend * 1e12,
+      value: weiAmountToSend,
     }
   );
   await sendTx.wait();
@@ -55,6 +52,22 @@ const main = async () => {
     "Deployer final balance:",
     ethers.formatEther(finalDeployerBalance),
     "ORAI"
+  );
+
+  const weiToSendNumber = +weiAmountToSend.toString();
+  const expectedBalanceAfterWithoutGasFees =
+    +deployerBalance.toString() - weiToSendNumber;
+
+  // just a rough estimate of the gas fees
+  const minExepectedBalanceAfterWithGasFees =
+    expectedBalanceAfterWithoutGasFees -
+    weiAmountToSend -
+    weiAmountToSend / 100000000;
+  assert(
+    +finalDeployerBalance.toString() <= expectedBalanceAfterWithoutGasFees
+  );
+  assert(
+    +finalDeployerBalance.toString() >= minExepectedBalanceAfterWithGasFees
   );
 
   // Check balance using contract method
